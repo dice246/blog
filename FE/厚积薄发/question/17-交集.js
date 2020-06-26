e1ff5093fabef5094a69b1747f010472ec5dd8d3
yinfan <yinfan@360.cn>
2020-06-26 09:25:08 +0800

    add: 交集

--- FE/厚积薄发/question/17-交集.js
+++ FE/厚积薄发/question/17-交集.js
@@ -1,3 +1,20 @@
 // 求多个数组之间的交集
+function intersection(arr1, arr2) {
+  let s1 = new Set([...arr1]);
+  let s2 = new Set([...arr2]);
+  let result = []
 
+  for (let item of s1.values()) {
+    if (s2.has(item)) {
+      result.push(item)
+    }
+  }
+
+  return result;
+}
+
+const arr1 = [1,2,3,4,5];
+const arr2 = [3,6,7];
+const result = intersection(arr1, arr2);
+console.log(result);
 
