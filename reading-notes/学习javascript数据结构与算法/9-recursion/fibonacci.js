efdf8bbbcdff083f2756d54b85da74565983042c
yinfan <yinfan@360.cn>
2020-06-27 11:17:38 +0800

    add: 第九章-斐波那契数列

--- reading-notes/学习javascript数据结构与算法/9-recursion/fibonacci.js
+++ reading-notes/学习javascript数据结构与算法/9-recursion/fibonacci.js
@@ -15,4 +15,25 @@ function fibonacci(n) {
   return fibonacci(n - 1) + fibonacci(n - 2)
 }
 
+/**
+ * 缓存
+ * @returns {fibonacci}
+ */
+function fibonacciMemoization() {
+  const memo = [0, 1];
+  const fibonacci = (n) => {
+    if (memo[n] != null) {
+      return memo[n]
+    }
+
+    return memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+  }
+
+  return fibonacci;
+}
+
 console.log(fibonacci(3))
+
+const f2 = fibonacciMemoization();
+console.log(f2(4));
+
